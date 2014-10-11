75eb7c83cd22bc57a2604f0a03f1faf098b1bbe8
Austin Hicks <camlorn38@gmail.com>
2014-10-11 14:34:25 -0400

    Renamed en ruleset to speech

--- mathml_accessibility/rulesets/speech/__init__.py
+++ mathml_accessibility/rulesets/speech/__init__.py
@@ -1,5 +1,5 @@
 from ...rule_set import *
 
-begin_rule_set("en")
+begin_rule_set("speech")
 add_topics("identifiers", "operators", "algebra", "calculus", "mathspeak_fallback")
 set_topic_order(["calculus", "algebra", "operators", "identifiers", "mathspeak_fallback"])
